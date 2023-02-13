6e204f859582f30b7f0534d8289dbcf5ab4adf1f
kekelberi <alexei.gaponov@gmail.com>
2023-02-12 18:46:16 -0600

    save

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,9 +1,12 @@
-import { Component } from "@angular/core";
+import { product } from './../../data/products';
+import { Component, Input } from '@angular/core';
+import { IProduct } from 'src/app/modules/product';
 
 @Component({
   selector: 'app-product',
-  templateUrl: './product.component.html'
+  templateUrl: './product.component.html',
 })
 export class ProductComponent {
-  
+  @Input() product: IProduct;
+  details = false;
 }
