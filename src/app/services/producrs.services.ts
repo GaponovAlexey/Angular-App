c5a6a978d23cfcd393de195414a0baa8bffb061d
kekelberi <alexei.gaponov@gmail.com>
2023-02-12 21:49:41 -0600

    save

--- src/app/services/producrs.services.ts
+++ src/app/services/producrs.services.ts
@@ -1,20 +1,32 @@
+import { ErrorService } from './error.service';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, delay } from 'rxjs';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
+import { Observable, delay, catchError, throwError } from 'rxjs';
 import { IProduct } from '../modules/product';
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsServices {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private errorService: ErrorService) {}
 
   getAll(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
-      params: new HttpParams({
-        fromObject: {limit: 5}
+    return this.http
+      .get<IProduct[]>('https://fakestoreapi.com/products', {
+        params: new HttpParams({
+          fromObject: { limit: 5 },
+        }),
       })
-    }).pipe(
-      delay(1000)
-    )
+      .pipe(delay(1000), catchError(this.errorHandler));
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+    this.errorService.handle(error.message);
+    return throwError(() => {
+      error.message;
+    });
   }
 }
